f60099ce8babe840e44650790be1dd0de5b7d920
kmejia-crawford <kmejia-crawford@unomaha.edu>
2022-02-26 14:07:24 -0600

    fixed the register

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-from rest_framework_jwt.serializers import User
 from .models import Movie
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -16,37 +16,35 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'},
+                                     validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'}, required=True)
 
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'password': {'write_only': True, 'min_length': 6},
+            'password2': {'write_only': True, 'min_length': 6}
+        }
 
-password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'},
-                                 validators=[validate_password])
-password2 = serializers.CharField(write_only=True, style={'input_type': 'password'}, required=True)
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
 
+        return attrs
 
-class Meta:
-    model = User
-    fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
-    extra_kwargs = {
-        'first_name': {'required': True},
-        'last_name': {'required': True}
-    }
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
 
+        user.set_password(validated_data['password'])
+        user.save()
 
-def validate(self, attrs):
-    if attrs['password'] != attrs['password2']:
-        raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-    return attrs
-
-
-def create(self, validated_data):
-    user = User.objects.create(
-        username=validated_data['username'],
-        email=validated_data['email'],
-        first_name=validated_data['first_name'],
-        last_name=validated_data['last_name'])
-
-    user.set_password(validated_data['password'])
-    user.save()
-
-    return user
+        return user
